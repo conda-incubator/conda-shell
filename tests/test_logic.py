5a0af471d78cdc20f9d6705abc9f469fb0b169f4
Katherine Abrikian <kalawac@users.noreply.github.com>
2023-07-21 06:03:13 -0400

    update osexec logic to call unchanged  methods directly from _Activator

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,9 +1,11 @@
 from argparse import Namespace
 from collections import namedtuple
-import inspect
 from pathlib import PurePath
+import inspect
 import os
 import pytest
+
+from conda.base.context import context, reset_context
 
 from condact.logic import PluginActivator, _ActivatorChild
 
@@ -162,8 +164,9 @@ def test_osexec_update_env_map_all(plugin_hook, monkeypatch):
     assert env_map["6"] == "False"
     assert env_map == current_env
 
+
 @pytest.mark.osexec
-def test_osexec_parse_and_build_cl_activate_cmd_dict(posix_ose_hook):
+def test_osexec_parse_and_build_activate_cmd_dict(posix_ose_hook):
     """
     Test that a namespace used to activate the base environment with an os.exec* hook
     returns a command dictionary with the expected keys and values.
@@ -171,14 +174,126 @@ def test_osexec_parse_and_build_cl_activate_cmd_dict(posix_ose_hook):
     ns = Namespace(command="activate", env=None, dev=False, stack=None)
     activator = PluginActivator(posix_ose_hook)
     builder = activator.parse_and_build(ns)
-    print(builder["export_vars"])
 
     # only export vars are CONDA_SHLVL and PATH
-    assert isinstance(builder["unset_vars"], tuple)
+    assert isinstance(builder["unset_vars"], tuple) or isinstance(builder["unset_vars"], list)
     assert isinstance(builder["set_vars"], dict)
     assert isinstance(builder["export_vars"], dict)
     assert int(builder["export_vars"]["CONDA_SHLVL"]) == 1
     assert "/" in builder["export_vars"]["PATH"]
+
+
+@pytest.mark.osexec
+def test_osexec_parse_and_build_reactivate_cmd_dict(posix_ose_hook):
+    """
+    Test that a namespace used to activate the base environment with an os.exec* hook
+    returns a command dictionary with the expected keys and values.
+    """
+    ns = Namespace(command="reactivate", dev=False)
+    activator = PluginActivator(posix_ose_hook)
+    builder = activator.parse_and_build(ns)
+
+    # only export vars are CONDA_SHLVL and PATH
+    assert isinstance(builder["unset_vars"], tuple) or isinstance(builder["unset_vars"], list)
+    assert isinstance(builder["set_vars"], dict)
+    assert isinstance(builder["export_vars"], dict)
+    assert int(builder["export_vars"]["CONDA_SHLVL"]) == 1
+    assert "/" in builder["export_vars"]["PATH"]
+
+@pytest.mark.osexec
+def test_osexec_parse_and_build_deactivate_cmd_dict(posix_ose_hook, monkeypatch):
+    """
+    Test that a namespace used to activate the base environment with an os.exec* hook
+    returns a command dictionary with the expected keys and values.
+    """
+    ns = Namespace(command="deactivate", dev=False)
+    activator = PluginActivator(posix_ose_hook)
+    builder = activator.parse_and_build(ns)
+    monkeypatch.setenv("CONDA_PREFIX", "base")
+    monkeypatch.setenv("CONDA_SHLVL", "0")
+
+    # only export vars are CONDA_SHLVL and PATH
+    assert isinstance(builder["unset_vars"], tuple) or isinstance(builder["unset_vars"], list)
+    assert isinstance(builder["set_vars"], dict)
+    assert isinstance(builder["export_vars"], dict)
+    assert isinstance(builder["deactivate_scripts"], tuple)
+    assert isinstance(builder["activate_scripts"], tuple)
+
+
+@pytest.mark.skip(reason="conda_cli generates a CondaValueError when looking for a solver")
+@pytest.mark.osexec
+def test_osexec_parse_and_build_deactivate_clean_env(posix_ose_hook, temp_env, conda_cli, monkeypatch):
+    """
+    Test that a namespace used to activate the base environment with an os.exec* hook
+    returns a command dictionary with the expected keys and values.
+    """
+    with temp_env:
+        conda_cli("activate", temp_env)
+        assert os.environ["CONDA_PREFIX"] == temp_env
+        breakpoint()
+        ns = Namespace(command="deactivate", dev=False)
+        activator = PluginActivator(posix_ose_hook)
+        builder = activator.parse_and_build(ns)
+        print(builder["export_vars"])
+
+        # only export vars are CONDA_SHLVL and PATH
+        assert isinstance(builder["unset_vars"], tuple)
+        assert isinstance(builder["set_vars"], dict)
+        assert isinstance(builder["export_vars"], dict)
+        assert int(builder["export_vars"]["CONDA_SHLVL"]) == 1
+        assert "/" in builder["export_vars"]["PATH"]
+        # assert "CONDA_PREFIX_1" in builder["unset_vars"] - only if stacked
+        assert "CONDA_PREFIX" in builder["export_vars"]
+        assert "CONDA_SHLVL" in builder["export_vars"]
+        assert "CONDA_DEFAULT_ENV" in builder["export_vars"]
+        assert "PATH" in builder["export_path"]
+
+
+@pytest.mark.osexec
+def test_osexec_build_deactivate_empty_cmd_dict_no_conda_shlvl(posix_ose_hook, monkeypatch):
+    """"
+    Test that build_deactivate returns an empty command dictionary when CONDA_SHLVL does not exist.
+    """
+    monkeypatch.delenv("CONDA_SHLVL")
+
+    activator = PluginActivator(posix_ose_hook)
+    builder = activator.build_deactivate()
+
+    assert isinstance(builder["unset_vars"], tuple)
+    assert isinstance(builder["set_vars"], dict)
+    assert isinstance(builder["export_vars"], dict)
+    assert isinstance(builder["deactivate_scripts"], tuple)
+    assert isinstance(builder["activate_scripts"], tuple)
+    assert not builder["unset_vars"]
+    assert not builder["set_vars"]
+    assert not builder["export_vars"]
+    assert not builder["deactivate_scripts"]
+    assert not builder["activate_scripts"]
+
+@pytest.mark.osexec
+@pytest.mark.parametrize("prefix, shlvl", [("", "2"), ("(abc) ", "0"), ("", "0")])
+def test_osexec_build_deactivate_empty_cmd_dict(prefix, shlvl, posix_ose_hook, monkeypatch):
+    """"
+    Test that build_deactivate returns an empty command dictionary when there is
+    either no CONDA_PREFIX or when CONDA_SHLVL is less than 1.
+    """
+    monkeypatch.setenv("CONDA_PREFIX", prefix)
+    monkeypatch.setenv("CONDA_SHLVL", shlvl)
+
+    activator = PluginActivator(posix_ose_hook)
+    builder = activator.build_deactivate()
+
+    assert isinstance(builder["unset_vars"], tuple)
+    assert isinstance(builder["set_vars"], dict)
+    assert isinstance(builder["export_vars"], dict)
+    assert isinstance(builder["deactivate_scripts"], tuple)
+    assert isinstance(builder["activate_scripts"], tuple)
+    assert not builder["unset_vars"]
+    assert not builder["set_vars"]
+    assert not builder["export_vars"]
+    assert not builder["deactivate_scripts"]
+    assert not builder["activate_scripts"]
+
 
 @pytest.mark.currentlogic
 def test_cl_init_happy_path(posix_cl_hook):
@@ -209,7 +324,7 @@ def test_cl_init_happy_path(posix_cl_hook):
 
 
 @pytest.mark.currentlogic
-def test_update_prompt_no_current_prompt_modifier(posix_cl_hook,  monkeypatch):
+def test_cl_update_prompt_no_current_prompt_modifier(posix_cl_hook,  monkeypatch):
     """
     Test that the correct prompt modifier is added to set_vars when there is no current
     prompt modifier.
@@ -223,3 +338,63 @@ def test_update_prompt_no_current_prompt_modifier(posix_cl_hook,  monkeypatch):
     activator._update_prompt(set_vars, "(hello) ")
 
     assert set_vars["PS1"] == "(hello) darkness, my old friend"
+
+
+@pytest.mark.currentlogic
+def test_cl_update_prompt_with_current_prompt_modifier(posix_cl_hook,  monkeypatch):
+    """
+    Test that the correct prompt modifier is added to set_vars when there is a current
+    prompt modifier.
+    """
+    monkeypatch.setenv("CONDA_PROMPT_MODIFIER", "(goodbye) ")
+    monkeypatch.setenv("PS1", "(goodbye) darkness, my old friend")
+    ns = Namespace(command="activate", env=None, dev=False, stack=None)
+    set_vars = {}
+
+    activator = _ActivatorChild(posix_cl_hook, ns)
+    activator._update_prompt(set_vars, "(hello) ")
+
+    assert set_vars["PS1"] == "(hello) darkness, my old friend"
+
+
+VALIDATE_PARSE_AND_SET_ARGS_TEST_CASES = (
+    (Namespace(command="activate", env=None, dev=False, stack=None),
+        1,
+        0,
+        ("activate", False, "base", True)),
+    (Namespace(command="activate", env="bronze", dev=True, stack=None),
+        0,
+        1,
+        ("activate", True, "bronze", False)),
+    (Namespace(command="deactivate", env="silver", dev=False, stack=True),
+        2,
+        1,
+        ("deactivate", False, "silver", True)),
+)
+
+@pytest.mark.skip(reason="context.dev not updating properly")
+@pytest.mark.currentlogic
+@pytest.mark.parametrize("ns, auto_stack, shlvl, expected", VALIDATE_PARSE_AND_SET_ARGS_TEST_CASES)
+def test_cl_parse_and_set_args(ns, auto_stack, shlvl, expected, posix_cl_hook, monkeypatch):
+    """
+    Test that the correct properties are set for various commands.
+    """
+    # setUp
+    monkeypatch.setenv("CONDA_AUTO_STACK", str(auto_stack))
+    monkeypatch.setenv("CONDA_SHLVL", str(shlvl))
+    reset_context()
+    assert context.auto_stack == auto_stack
+    assert context.shlvl == shlvl
+
+    # test
+    activator = _ActivatorChild(posix_cl_hook, ns)
+    activator._parse_and_set_args(ns)
+
+    assert activator.command == expected[0]
+    assert context.dev is expected[1]
+    assert activator.env_name_or_prefix == expected[2]
+    assert bool(activator.stack) is expected[3]
+
+    # tearDown
+    reset_context()
+
